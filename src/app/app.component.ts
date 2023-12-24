9c6aab430e0b93a86976db36392a509f4dc43a1f
gabthadon <dzangalist@gmail.com>
2023-12-24 20:49:31 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'QuizeApp';
+
+  questions  = [
+    {id: 1,
+     question:"What is the capital of Nigeria",
+  optionA:  "A Abuja",
+   optionB:"B Lagos",
+    optionC :"C Kano",
+     optionD :"D Ibadan",
+     answer:"A Abuja"
+    },
+    
+  {
+    id:2,
+    question:"Who is the president of Nigeria",
+    optionA: "A Goodluck Jonathan", 
+    optionB : "B Gen Yakubu Gowon",
+     optionC: "C Bola Ahmed Tinumbu", 
+     optionD :"D Musa Yaradua",
+     answer: "C Bola Ahmed Tinumbu"
+  },
+
+    {
+    id:3,
+    question:"What is the capital of Kenya",
+    optionA: "A Kieve", 
+    optionB: "B Abuja",
+    optionC: "C Jakarta",
+    optionD: "D Nairobi",
+    answer:"D Nairobi"
+  },
+
+  {
+    id:4,
+    question:"Who won the FIFA world cup 2022",
+    optionA: "A Morocco", 
+    optionB: "B Argentina", 
+    optionC: "C France", 
+    optionD: "D Germany",
+    answer:"B Argentina"
+  },
+
+  {
+    id:5,
+    question:"Who hosted the 2022  FIFA world cup",
+    optionA: "A Japan",
+     optionB:  "B Brazil",
+      optionC: "C Quarta", 
+      optionD: "D Italy",
+      answer:"C Quarta"
+  },
+  ]
+ 
+  selectedOption="";
+   public solution(index:any){
+  let count:number=0;
+if(this.selectedOption == this.questions[index].answer){
+  count++
+}
+console.log("Score = " + count);
+
+   }
 }
